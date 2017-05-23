f0f1d0b7e912dce2f261fdaea92e8ab3100b8fd2
Cam Wiegert <cam@camwiegert.com>
2017-05-23 12:03:00 -0500

    Add tests for DOM module

---
+++ test/dom.test.js
@@ -0,0 +1,54 @@
+import getElements from '../src/dom';
+import jQuery from 'jquery';
+
+describe('getElements', () => {
+
+  test('returns empty array by default', () => {
+    expect(getElements())
+      .toEqual([]);
+    expect(getElements(false))
+      .toEqual([]);
+  });
+
+  test('handles selector strings', () => {
+    expect(getElements('body'))
+      .toHaveLength(1);
+  });
+
+  test('handles element arrays', () => {
+    expect(getElements([
+      document.body,
+      document.head
+    ])).toHaveLength(2);
+  });
+
+  test('handles HTMLCollection', () => {
+    expect(getElements(document.forms))
+      .toBeInstanceOf(Array);
+  });
+
+  test('handles NodeList', () => {
+    const selection = getElements(document.querySelectorAll('body'));
+    expect(selection)
+      .toBeInstanceOf(Array);
+    expect(selection)
+      .toHaveLength(1);
+  });
+
+  test('handles a single node', () => {
+    const selection = getElements(document.body);
+    expect(selection)
+      .toBeInstanceOf(Array);
+    expect(selection)
+      .toHaveLength(1);
+  });
+
+  test('handles jQuery objects', () => {
+    const selection = getElements(jQuery('body'));
+    expect(selection)
+      .toBeInstanceOf(Array);
+    expect(selection)
+      .toHaveLength(1);
+  });
+
+});
