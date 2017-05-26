6117d3e98ae21b66db2373dace691a4acce04270
Cam Wiegert <cam@camwiegert.com>
2017-05-26 11:11:55 -0500

    Add Registry.on and handler store

--- src/registry.js
+++ src/registry.js
@@ -6,6 +6,7 @@ export default class Registry {
   constructor(elements, options) {
     this.elements = getElements(elements);
     this.mapCache = this.elements.map(el => false);
+    this.handlers = { enter: [], exit: [] };
     this.options = {
       ...defaults,
       ...options
@@ -28,6 +29,18 @@ export default class Registry {
 
   report(element, entered, exited) {
     // Call appropriate handlers...
+  }
+
+  on(event, fn) {
+    if (event !== 'enter' && event !== 'exit')
+      throw new Error(
+        'Unsupported event type. Expected "enter" or "exit".'
+      );
+    this.handlers[event].push(fn);
+    return () => {
+      const index = this.handlers[event].indexOf(fn);
+      this.handlers[event].splice(index, 1);
+    };
   }
 
   getMap() {
