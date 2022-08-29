921b0d796742eafa6a59bf2d68599c387ba507ce
Torsten Dunkel <Torsten.Dunkel@goldbach.com>
2022-08-29 16:54:14 +0200

    updated webpack to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const webpack = require('webpack'),
-      path    = require('path'),
-      package = require('./package');
-
+    path = require('path'),
+    package = require('./package');
 const banner = `${package.name} ${package.version} - ${package.description}\nCopyright (c) ${ new Date().getFullYear() } ${package.author} - ${package.homepage}\nLicense: ${package.license}`;
-
 module.exports = {
     'context': path.join(__dirname, '/src'),
     'entry': './index.js',
@@ -14,13 +12,19 @@ module.exports = {
         'libraryTarget': 'umd'
     },
     'module': {
-        'loaders': [{
+        rules: [{
             'test': /\.js$/,
             'exclude': /node_modules/,
-            'loader': 'babel'
+            'loader': 'babel-loader',
+            options: {
+                presets: [
+                    ["@babel/preset-env", { "targets": "> 0.25% and not dead and not op_mini all and not android 4.4.3-4.4.4" }]
+                ]
+            }
         }]
     },
+
     'plugins': [
-        new webpack.BannerPlugin(banner)
-    ]
+    new webpack.BannerPlugin(banner)
+]
 };
